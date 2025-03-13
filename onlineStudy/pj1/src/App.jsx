076a75e08d7129f2e661ab966d23b77a227bb92e
jema <smh1141@naver.com>
2025-03-13 17:11:24 +0900

    tabs를 반복하는 문법 적용

--- onlineStudy/pj1/src/App.jsx
+++ onlineStudy/pj1/src/App.jsx
@@ -1,6 +1,9 @@
+// Hooks
 import { useState } from "react";
-
+// Data
 import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data-with-examples.js";
+// Components
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
@@ -32,10 +35,13 @@ function App() {
         <section id="examples">
             <h2>Examples</h2>
             <menu>
-              <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+              {Object.keys(EXAMPLES).map(key => (
+                <TabButton onSelect={() => handleSelect(key)} key={key}>{key}</TabButton>
+              ))}
+              {/* <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
               <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
               <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-              <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+              <TabButton onSelect={() => handleSelect('State')}>State</TabButton> */}
             </menu>
             {tabContent}
         </section>
