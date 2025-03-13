238ec4e37a74323797c7fb52901c0aa09df95e04
jema <smh1141@naver.com>
2025-03-13 20:57:18 +0900

    Examples 컴포넌트로 분리

--- onlineStudy/pj1/src/App.jsx
+++ onlineStudy/pj1/src/App.jsx
@@ -1,67 +1,15 @@
-// Hooks
-import { useState } from "react";
-// Data
-import { EXAMPLES } from "./data-with-examples.js";
 // Components
+import Examples from "./components/Examples.jsx";
 import Header from "./components/Header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
 import CoreConcepts from "./components/coreConcepts.jsx";
 
 function App() {
-  const [tabContent, setTabContent] = useState("");
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'Components' , 'Jsx', 'Props', 'State' 중 하나
-    setTabContent(selectedButton);
-  }
-
-  let tabViewContent = <p>Please select a topic.</p>;
-
-  if (tabContent) {
-    tabViewContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[tabContent].title}</h3>
-        <p>{EXAMPLES[tabContent].description}</p>
-        <pre>
-          <code>{EXAMPLES[tabContent].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
     <>
       <Header />
       <main>
         <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            {Object.keys(EXAMPLES).map((key) => (
-              <TabButton
-                isSelected={tabContent === key}
-                onSelect={() => handleSelect(key)}
-                key={key}
-              >
-                {key}
-              </TabButton>
-            ))}
-            {/* <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
-              <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-              <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-              <TabButton onSelect={() => handleSelect('State')}>State</TabButton> */}
-          </menu>
-          {tabViewContent}
-          {/* {!tabContent ? <p>Please select a topic.</p> :(
-            <div id="tab-content">
-              <h3>{EXAMPLES[tabContent].title}</h3>
-              <p>{EXAMPLES[tabContent].description}</p>
-              <pre>
-                <code>{EXAMPLES[tabContent].code}</code>
-              </pre>
-            </div>
-            )} */}
-        </section>
+        <Examples />
       </main>
     </>
   );
---
+++ onlineStudy/pj1/src/components/Examples.jsx
@@ -0,0 +1,62 @@
+// Hooks
+import { useState } from "react";
+//components
+import TabButton from "./TabButton";
+//data
+import { EXAMPLES } from "../data-with-examples";
+
+export default function Examples() {
+  const [tabContent, setTabContent] = useState("");
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'Components' , 'Jsx', 'Props', 'State' 중 하나
+    setTabContent(selectedButton);
+  }
+
+  let tabViewContent = <p>Please select a topic.</p>;
+
+  if (tabContent) {
+    tabViewContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[tabContent].title}</h3>
+        <p>{EXAMPLES[tabContent].description}</p>
+        <pre>
+          <code>{EXAMPLES[tabContent].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        {Object.keys(EXAMPLES).map((key) => (
+          <TabButton
+            isSelected={tabContent === key}
+            onSelect={() => handleSelect(key)}
+            key={key}
+          >
+            {key}
+          </TabButton>
+        ))}
+      </menu>
+      {tabViewContent}
+    </section>
+  );
+}
+
+/* <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+    <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+    <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+    <TabButton onSelect={() => handleSelect('State')}>State</TabButton> */
+
+/* {!tabContent ? <p>Please select a topic.</p> :(
+    <div id="tab-content">
+    <h3>{EXAMPLES[tabContent].title}</h3>
+    <p>{EXAMPLES[tabContent].description}</p>
+    <pre>
+        <code>{EXAMPLES[tabContent].code}</code>
+    </pre>
+    </div>
+    )} */
