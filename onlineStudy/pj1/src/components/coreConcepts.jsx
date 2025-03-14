c32518e82f8e9237b38191cc836a65b4a3445ccd
jema <smh1141@naver.com>
2025-03-14 16:46:08 +0900

    spread props와 distructuring에 대한 이해2

--- onlineStudy/pj1/src/components/coreConcepts.jsx
+++ onlineStudy/pj1/src/components/coreConcepts.jsx
@@ -8,8 +8,7 @@ import Section from "./Section";
 
 export default function CoreConcepts() {
   return (
-    <Section id="core-concepts">
-      <h2>Core Concept</h2>
+    <Section id="core-concepts" title="Core Concept">
       <ul>
         {CORE_CONCEPTS.map((concept) => (
           <CoreConcept key={concept.title} {...concept} />
